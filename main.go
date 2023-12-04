907fa5076daa90f092c648da61b0079fa13b52fa
ashish-7711-cse <84677550+ashish-7711-cse@users.noreply.github.com>
2023-12-04 08:31:09 +0530

    Updated the new code

--- main.go
+++ main.go
@@ -1,59 +1,3 @@
-// package main
-
-// import "fmt"
-
-// func main() {
-// 	adidasFactory, _ := GetSportsFactory("adidas")
-// 	nikeFactory, _ := GetSportsFactory("nike")
-
-// 	nikeShoe := nikeFactory.makeShoe()
-// 	nikeShirt := nikeFactory.makeShirt()
-
-// 	adidasShoe := adidasFactory.makeShoe()
-// 	adidasShirt := adidasFactory.makeShirt()
-
-// 	printShoeDetails(nikeShoe)
-// 	printShirtDetails(nikeShirt)
-
-// 	printShoeDetails(adidasShoe)
-// 	printShirtDetails(adidasShirt)
-// }
-
-// func printShoeDetails(s IShoe) {
-// 	fmt.Printf("Logo: %s", s.getLogo())
-// 	fmt.Println()
-// 	fmt.Printf("Size: %d", s.getSize())
-// 	fmt.Println()
-// }
-
-// func printShirtDetails(s IShirt) {
-// 	fmt.Printf("Logo: %s", s.getLogo())
-// 	fmt.Println()
-// 	fmt.Printf("Size: %d", s.getSize())
-// 	fmt.Println()
-// }
-
-package main
-
-import (
-	"fmt"
-)
-
-// func main() {
-// 	ak47, _ := getGun("ak47")
-// 	musket, _ := getGun("musket")
-
-// 	printDetails(ak47)
-// 	printDetails(musket)
-// }
-
-// func printDetails(g IGun) {
-// 	fmt.Printf("Gun: %s", g.getName())
-// 	fmt.Println()
-// 	fmt.Printf("Power: %d", g.getPower())
-// 	fmt.Println()
-// }
-
 type InputValues struct {
 	id    string
 	value string
@@ -61,7 +5,7 @@ type InputValues struct {
 
 type ProvisionInputCommon struct {
 	Network []InputValues
-	Volumes string
+	Volumes []InputValues
 	Name    string
 	Version string
 }
@@ -71,23 +15,23 @@ type InputStruct struct {
 	pass     string
 }
 
-type InstanceResponse struct {
+type Response struct {
 	RespStatusCode int
 	ResponseBytes  []byte
 }
 
 type ProvisionContainerInterface interface {
-	Provision() InstanceResponse
-	start() InstanceResponse
-	stop() InstanceResponse
-	delete() InstanceResponse
+	Provision() Response
+	start() Response
+	stop() Response
+	delete() Response
 }
 
 type Instance struct {
 	ProvisionInput ProvisionInputCommon
 	Instance_ID    string
 	Peer_ID        string
-	Configversion  string
+	ConfigVersion  string
 }
 
 type activeInstance struct {
@@ -98,9 +42,11 @@ type standbyInstance struct {
 	Instance
 }
 
-func newActive() ProvisionContainerInterface {
+func newActive(input InputStruct) ProvisionContainerInterface {
 	return &activeInstance{
-		Instance: Instance{},
+		Instance: Instance{
+			Instance_ID: input.userName,
+		},
 	}
 }
 
@@ -110,33 +56,30 @@ func newStandby() ProvisionContainerInterface {
 	}
 }
 
-func (i *Instance) start() InstanceResponse {
+func (i *Instance) start() Response {
 	fmt.Println("Started")
-	createResp := InstanceResponse{}
+	createResp := Response{}
 	createResp.RespStatusCode = 200
-
 	return createResp
 }
 
-func (i *Instance) stop() InstanceResponse {
+func (i *Instance) stop() Response {
 	fmt.Println("Stopped")
-	createResp := InstanceResponse{}
+	createResp := Response{}
 	createResp.RespStatusCode = 200
-
 	return createResp
 }
 
-func (i *Instance) delete() InstanceResponse {
+func (i *Instance) delete() Response {
 	fmt.Println("Deleted")
-	createResp := InstanceResponse{}
+	createResp := Response{}
 	createResp.RespStatusCode = 200
-
 	return createResp
 }
 
 func getInstanceFactory(instanceType string, input InputStruct) (ProvisionContainerInterface, error) {
 	if instanceType == "active" {
-		return newActive(), nil
+		return newActive(input), nil
 	} else if instanceType == "standby" {
 		// In case of standby instance, userInput will have additional attributes like peer-id and replica-id
 		return newStandby(), nil
@@ -144,17 +87,15 @@ func getInstanceFactory(instanceType string, input InputStruct) (ProvisionContai
 	return nil, fmt.Errorf("Wrong gun type passed")
 }
 
-func (a *activeInstance) Provision() InstanceResponse {
-
-	createResp := InstanceResponse{}
+func (a *activeInstance) Provision() Response {
+	createResp := Response{}
 	createResp.RespStatusCode = 200
 	fmt.Println("Active Created ______")
 	return createResp
 }
 
-func (s *standbyInstance) Provision() InstanceResponse {
-
-	createResp := InstanceResponse{}
+func (s *standbyInstance) Provision() Response {
+	createResp := Response{}
 	createResp.RespStatusCode = 400
 	fmt.Println("Standby Created_____")
 	return createResp
@@ -163,8 +104,7 @@ func (s *standbyInstance) Provision() InstanceResponse {
 func main() {
 	active, _ := getInstanceFactory("active", InputStruct{})
 	active.Provision()
-
 	standby, _ := getInstanceFactory("standby", InputStruct{})
 	standby.Provision()
-
 }
+
